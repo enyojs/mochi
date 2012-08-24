b2cddc214a8ca8b2226612a4a36edb57c945a2ee
germboy <jeremy.t@verizon.net>
2012-08-24 13:04:22 -0400

    Update ToggleButton sample
    Enyo-DCO-1.0-Signed-off-by: Jeremy Thomas <jeremy.thomas@palm.com>

--- samples/ToggleButtonSample.js
+++ samples/ToggleButtonSample.js
@@ -2,38 +2,42 @@ enyo.kind({
 	name: "mochi.sample.ToggleButtonSample",
 	classes: "mochi mochi-sample",
 	components: [
-		{classes: "mochi-subheader", content: "Toggle Buttons"},
+		{classes: "mochi-subheader", content: "Toggle Buttons: Default"},
 		{classes: "mochi-sample-tools", components: [
-			{kind:"mochi.ToggleButton", onChange:"toggleChanged", value: true},
-			{kind:"mochi.ToggleButton", onChange:"toggleChanged"},
-			{kind:"mochi.ToggleButton", onChange:"toggleChanged"},
-			{kind:"mochi.ToggleButton", onChange:"toggleChanged", value: true, disabled: true}
+			{kind: "mochi.ToggleButton", onChange: "toggleChanged", value: true},
+			{kind: "mochi.ToggleButton", onChange: "toggleChanged"},
+			{kind: "mochi.ToggleButton", onChange: "toggleChanged"}
+		]},
+		{tag: "br"},
+		{classes: "mochi-subheader", content: "Toggle Buttons: Disabled"},
+		{classes: "mochi-sample-tools", components: [
+			{kind: "mochi.ToggleButton", onChange: "toggleChanged", disabled: true},
+			{kind: "mochi.ToggleButton", onChange: "toggleChanged", value: true, disabled: true}
+		]},
+		{tag: "br"},
+		{classes: "mochi-subheader", content: "Toggle Buttons: Custom Colors"},
+		{classes: "mochi-sample-tools", components: [
+			{kind: "mochi.ToggleButton", onChange: "toggleChanged", colorActive: "#69cdff", colorInactive: "#ff4a4a"},
+			{kind: "mochi.ToggleButton", onChange: "toggleChanged", colorActive: "#69cdff", colorInactive: "#ff4a4a", value: true}
 		]},
 		{tag: "br"},
 		{classes: "mochi-subheader", content: "Toggle Buttons Group"},
-		{kind: "Group", classes: "mochi-sample-tools group", onActivate:"groupActivated", highlander: true, components: [
-			{kind:"mochi.ToggleButton"},
-			{kind:"mochi.ToggleButton", value: true, colorActive: "#69cdff", colorInactive: "#000"},
-			{kind:"mochi.ToggleButton"}
-		]}
-		/*
+		{kind: "Group", classes: "mochi-sample-tools group", onActivate: "groupActivated", highlander: true, components: [
+			{kind: "mochi.ToggleButton"},
+			{kind: "mochi.ToggleButton", value: true},
+			{kind: "mochi.ToggleButton"}
+		]},
 		{tag: "br"},
-		{kind: "mochi.Groupbox", classes:"mochi-sample-result-box", components: [
-			{kind: "mochi.GroupboxHeader", content: "Result"},
-			{name:"result", classes:"mochi-sample-result", content:"No button tapped yet."}
-		]}
-		*/
+		{name: "result", classes: "mochi-sample-content", content: "No button tapped yet."}
 	],
 	toggleChanged: function(inSender, inEvent) {
-		//this.$.result.setContent(inSender.name + " was " + (inSender.getValue() ? " selected." : "deselected."));
+		this.$.result.setContent(inSender.name + " was " + (inSender.getValue() ? " selected." : "deselected."));
 	},
 	ordinals: ["1st", "2nd", "3rd"],
 	groupActivated: function(inSender, inEvent) {
-		/*
 		if (inEvent.originator.getActive()) {
 			var selected = inEvent.originator.indexInContainer();
 			this.$.result.setContent("The " + this.ordinals[selected] + " toggle button in the group is selected.");
 		}
-		*/
 	}
 });
