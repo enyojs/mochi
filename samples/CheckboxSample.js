c8661156d0ea328b67784f4e7dc78769e1454fd2
Jeremy Thomas <jeremy.t@verizon.net>
2012-11-02 15:14:09 -0700

    Add Scroller to Checkbox sample
    Enyo-DCO-1.1-Signed-off-by: Jeremy Thomas <jeremy.thomas@palm.com>

--- samples/CheckboxSample.js
+++ samples/CheckboxSample.js
@@ -1,37 +1,40 @@
 enyo.kind({
 	name: "mochi.sample.CheckboxSample",
-	classes: "mochi mochi-sample",
+	classes: "mochi mochi-sample enyo-fit",
+	kind: "FittableRows",
 	components: [
-		{classes: "mochi-subheader", content: "Checkbox: Default"},
-		{classes: "mochi-sample-tools", components: [
-			{kind: "mochi.Checkbox", onchange: "sample1Changed", checked: true},
-			{kind: "mochi.Checkbox", onchange: "sample1Changed"},
-			{kind: "mochi.Checkbox", onchange: "sample1Changed"},
-			{name: "sample1Result", classes:"mochi-sample-content", content:"No button tapped yet."}
-		]},
-		{tag: "br"},
-		{classes: "mochi-subheader", content: "Checkboxes: Disabled"},
-		{classes: "mochi-sample-tools", components: [
-			{kind: "mochi.Checkbox", checked: true, disabled: true},
-			{kind: "mochi.Checkbox", disabled: true},
-			{kind: "mochi.Checkbox", disabled: true}
-		]},
-		{tag: "br"},
-		{classes: "mochi-subheader", content: "Checkboxes: Custom Styles"},
-		{classes: "mochi-sample-tools", components: [
-			{kind: "mochi.Checkbox", onchange: "sample2Changed", colorActive: "#8c3037", colorInactive: "#d9d4ba", checked: true},
-			{kind: "mochi.Checkbox", onchange: "sample2Changed", colorActive: "#E75533", colorInactive: "#fafafa", checked: true},
-			{kind: "mochi.Checkbox", onchange: "sample2Changed", colorActive: "#364b57", colorInactive: "#fafafa", checked: true},
-			{name: "sample2Result", classes:"mochi-sample-content", content:"No button tapped yet."}
-		]},
-		{tag: "br"},
-		{classes: "mochi-subheader", content: "Checkbox Group"},
-		{kind: "Group", classes: "mochi-sample-tools group", onActivate: "sample3Activated", highlander: true, components: [
-			{kind: "mochi.Checkbox", checked: true},
-			{kind: "mochi.Checkbox"},
-			{kind: "mochi.Checkbox"}
-		]},
-		{name: "sample3Result", classes: "mochi-sample-content", content: "No button tapped yet."}
+		{kind: "enyo.Scroller", fit: true, components:[
+			{classes: "mochi-subheader", content: "Checkbox: Default"},
+			{classes: "mochi-sample-tools", components: [
+				{kind: "mochi.Checkbox", onchange: "sample1Changed", checked: true},
+				{kind: "mochi.Checkbox", onchange: "sample1Changed"},
+				{kind: "mochi.Checkbox", onchange: "sample1Changed"},
+				{name: "sample1Result", classes:"mochi-sample-content", content:"No button tapped yet."}
+			]},
+			{tag: "br"},
+			{classes: "mochi-subheader", content: "Checkboxes: Disabled"},
+			{classes: "mochi-sample-tools", components: [
+				{kind: "mochi.Checkbox", checked: true, disabled: true},
+				{kind: "mochi.Checkbox", disabled: true},
+				{kind: "mochi.Checkbox", disabled: true}
+			]},
+			{tag: "br"},
+			{classes: "mochi-subheader", content: "Checkboxes: Custom Styles"},
+			{classes: "mochi-sample-tools", components: [
+				{kind: "mochi.Checkbox", onchange: "sample2Changed", colorActive: "#8c3037", colorInactive: "#d9d4ba", checked: true},
+				{kind: "mochi.Checkbox", onchange: "sample2Changed", colorActive: "#E75533", colorInactive: "#fafafa", checked: true},
+				{kind: "mochi.Checkbox", onchange: "sample2Changed", colorActive: "#364b57", colorInactive: "#fafafa", checked: true},
+				{name: "sample2Result", classes:"mochi-sample-content", content:"No button tapped yet."}
+			]},
+			{tag: "br"},
+			{classes: "mochi-subheader", content: "Checkbox Group"},
+			{kind: "Group", classes: "mochi-sample-tools group", onActivate: "sample3Activated", highlander: true, components: [
+				{kind: "mochi.Checkbox", checked: true},
+				{kind: "mochi.Checkbox"},
+				{kind: "mochi.Checkbox"}
+			]},
+			{name: "sample3Result", classes: "mochi-sample-content", content: "No button tapped yet."}
+		]}
 	],
 	sample1Changed: function(inSender, inEvent) {
 		this.$.sample1Result.setContent(inSender.name + " was " + (inSender.getValue() ? " selected." : "deselected."));
