296392c46437f57947e1fab1fda5ec1fcaa80450
germboy <jeremy.t@verizon.net>
2012-12-06 14:21:42 -0500

    Update Samples
    Enyo-DCO-1.1-Signed-off-by: Jeremy Thomas <jeremy.thomas@palm.com>

--- samples/BadgeSample.js
+++ samples/BadgeSample.js
@@ -1,50 +1,65 @@
 enyo.kind({
 	name: "mochi.sample.BadgeSample",
-	classes: "mochi mochi-sample",
+	classes: "mochi mochi-sample enyo-fit",
+	kind: "FittableRows",
 	components: [
-		{classes: "mochi-subheader", content: "Stand-alone Badge"},
-		{name: "badge", kind: "mochi.Badge", content: "25"},
-		{
-			content: "Change badge value", 
-			components: [
-				{name: "slider", kind: "mochi.Slider", value: 25, onChanging:"sliderChanging", onChange:"sliderChanged"}
-			]
-		},
-		{tag: "br"},
+		{kind: "enyo.Scroller", fit: true, components:[
 
-		{classes: "mochi-subheader", content: "Badge inside a button"},
-		{
-			kind: "mochi.Button", content: "Button", components: [
-				{kind: "mochi.Badge", content: "56"},
-			]
-		},
-		{tag: "br"},
+			{kind: "mochi.Subheader", content: "Stand-alone Badge"},
+			{classes: "mochi-sample-tools", components: [
+				{name: "badge", kind: "mochi.Badge", content: "25"},
+				{
+					content: "Change badge value", 
+					components: [
+						{name: "slider", kind: "mochi.Slider", value: 25, onChanging:"sliderChanging", onChange:"sliderChanged"}
+					]
+				}
+			]},
+			{tag: "br"},
 
-		{classes: "mochi-subheader", content: "Badge inside a toolbar"},
-		{
-			kind: "onyx.Toolbar", components: [
-				{content: "Toolbar"},
-				{kind: "mochi.Badge", content: "11"},
-			]
-		},
-		{tag: "br"},
+			{kind: "mochi.Subheader", content: "Badge inside a button"},
+			{classes: "mochi-sample-tools", components: [
+				{
+					kind: "mochi.Button", content: "Button", components: [
+						{kind: "mochi.Badge", content: "56"},
+					]
+				}
+			]},
+			{tag: "br"},
+
+			{kind: "mochi.Subheader", content: "Badge inside a toolbar"},
+			{
+				kind: "onyx.Toolbar", components: [
+					{content: "Toolbar"},
+					{kind: "mochi.Badge", content: "11"},
+				]
+			},
+			{tag: "br"},
 
 
-		{classes: "mochi-subheader", content: "Colorful Badges"},
-		{kind: "mochi.Badge", content: "6", background: "red"},
-		{kind: "mochi.Badge", content: "88", background: "orange"},
-		{kind: "mochi.Badge", content: "3", background: "green"},
+			{kind: "mochi.Subheader", content: "Colorful Badges"},
+			{classes: "mochi-sample-tools", components: [
+				{kind: "mochi.Badge", content: "6", background: "red"},
+				{kind: "mochi.Badge", content: "88", background: "orange"},
+				{kind: "mochi.Badge", content: "3", background: "green"}
+			]},
 
-		/*{classes: "mochi-subheader", content: "Text Badges"},
-		{kind: "mochi.Badge", content: "Error! Please check your email address.", background: "red"},
-		{tag: "br"},
-		{kind: "mochi.Badge", content: "Warning! Please save your changes before proceeding.", background: "orange"},
-		{tag: "br"},
-		{kind: "mochi.Badge", content: "Success! Your changes have been saved.", background: "green"},
-		*/
-		
-		{classes: "mochi-subheader", content: "Badge with a large number"},
-		{kind: "mochi.Badge", content: "2012"}
+			/*{kind: "mochi.Subheader", content: "Text Badges"},
+			{classes: "mochi-sample-tools", components: [
+				{kind: "mochi.Badge", content: "Error! Please check your email address.", background: "red"},
+				{tag: "br"},
+				{kind: "mochi.Badge", content: "Warning! Please save your changes before proceeding.", background: "orange"},
+				{tag: "br"},
+				{kind: "mochi.Badge", content: "Success! Your changes have been saved.", background: "green"}
+			]},
+			*/
+			
+			{kind: "mochi.Subheader", content: "Badge with a large number"},
+			{classes: "mochi-sample-tools", components: [
+				{kind: "mochi.Badge", content: "2012"}
+			]}
+
+		]}
 	],
 	sliderChanging: function(inSender, inEvent) {
 		this.$.badge.setContent(Math.round(inSender.getValue()));
--- samples/RadioButtonSample.js
+++ samples/RadioButtonSample.js
@@ -1,30 +1,34 @@
 enyo.kind({
 	name: "mochi.sample.RadioButtonSample",
 	classes: "mochi mochi-sample enyo-unselectable enyo-fit",
+	kind: "FittableRows",
 	components: [
-		{classes: "mochi-subheader", content: "RadioButtons: Default"},
-		{classes: "mochi-sample-tools", components: [
-			{kind: "mochi.RadioButton", onchange: "sample1Changed", checked: true},
-			{kind: "mochi.RadioButton", onchange: "sample1Changed"},
-			{kind: "mochi.RadioButton", onchange: "sample1Changed"},
-			{name: "sample1Result", classes: "mochi-sample-content", content: "No button tapped yet."}
-		]},
-		{tag: "br"},
-		{classes: "mochi-subheader", content: "RadioButtons: Disabled"},
-		{classes: "mochi-sample-tools", components: [
-			{kind: "mochi.RadioButton", onchange: "sample2Changed", disabled: true},
-			{kind: "mochi.RadioButton", onchange: "sample2Changed", disabled: true, checked: true},
-			{name: "sample2Result", classes: "mochi-sample-content", content: "No button tapped yet."}
-		]},
-		{tag: "br"},
-		{classes: "mochi-subheader", content: "RadioButton Group"},
-		{kind: "Group", classes: "mochi-sample-tools group", onActivate: "sample3Activated", highlander: true, components: [
-			{kind: "mochi.RadioButton", checked: true},
-			{kind: "mochi.RadioButton"},
-			{kind: "mochi.RadioButton"}
-		]},
-		{name: "sample3Result", classes: "mochi-sample-content", content: "No button tapped yet."}
-		
+		{kind: "enyo.Scroller", fit: true, components:[
+			{kind: "mochi.Subheader", content: "RadioButtons: Default"},
+			{classes: "mochi-sample-tools", components: [
+				{kind: "mochi.RadioButton", onchange: "sample1Changed", checked: true},
+				{kind: "mochi.RadioButton", onchange: "sample1Changed"},
+				{kind: "mochi.RadioButton", onchange: "sample1Changed"},
+				{name: "sample1Result", classes: "mochi-sample-content", content: "No button tapped yet."}
+			]},
+			{tag: "br"},
+			{kind: "mochi.Subheader", content: "RadioButtons: Disabled"},
+			{classes: "mochi-sample-tools", components: [
+				{kind: "mochi.RadioButton", onchange: "sample2Changed", disabled: true},
+				{kind: "mochi.RadioButton", onchange: "sample2Changed", disabled: true, checked: true},
+				{name: "sample2Result", classes: "mochi-sample-content", content: "No button tapped yet."}
+			]},
+			{tag: "br"},
+			{kind: "mochi.Subheader", content: "RadioButton Group"},
+			{kind: "Group", classes: "mochi-sample-tools group", onActivate: "sample3Activated", highlander: true, components: [
+				{kind: "mochi.RadioButton", checked: true},
+				{kind: "mochi.RadioButton"},
+				{kind: "mochi.RadioButton"}
+			]},
+			{classes: "mochi-sample-tools", components: [
+				{name: "sample3Result", classes: "mochi-sample-content", content: "No button tapped yet."}
+			]}
+		]}
 	],
 	sample1Changed: function(inSender, inEvent) {
 		this.$.sample1Result.setContent(inSender.name + " was " + (inSender.getValue() ? " selected." : "deselected."));
